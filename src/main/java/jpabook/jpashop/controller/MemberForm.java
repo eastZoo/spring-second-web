f4cb20c107c0948720d243e79f919bb49711a453
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-02-07 17:43:03 +0900

    feat : add memberFormUI

--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -7,11 +7,12 @@ import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class MemberForm {
-
     @NotEmpty(message = "회원 이름은 필수 입니다.")
     private String name;    // javax validation을 통해서 spring이 값이 비어있으면 오류 띄워줌
 
     private String city;
+
     private String street;
+
     private String zipcode;
 }
